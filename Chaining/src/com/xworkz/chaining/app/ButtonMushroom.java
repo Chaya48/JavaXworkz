519c0190b8c4d647d9372583cac639fce1b07ef4
Chaya48 <chayakm.xworkz@gmail.com>
2023-11-05 21:20:35 +0530

    chaining

--- Chaining/src/com/xworkz/chaining/app/ButtonMushroom.java
+++ Chaining/src/com/xworkz/chaining/app/ButtonMushroom.java
@@ -1,14 +1,14 @@
 package com.xworkz.chaining.app;
 
-public class ButtonMushroom extends Mushroom{
+public class ButtonMushroom extends Mushroom {
 
 	public ButtonMushroom() {
-		super("Beech",20,60,'s');
+		super("Beech", 20, 60, 's');
 		System.out.println("invoking no- args constructor in button mushroom");
 	}
-	
-	public ButtonMushroom(String type,int number, double price, char size) {
-	super(type,number,price,size);
-   System.out.println("invoking string,int,double,char constructor in button mushroom");
+
+	public ButtonMushroom(String type, int number, double price, char size) {
+		super(type, number, price, size);
+		System.out.println("invoking string,int,double,char constructor in button mushroom");
 	}
 }
